0595211069700f5b8e2ac7da6b9929b187c0e1fc
Michal Swietek <mike1729@users.noreply.github.com>
2023-03-08 13:17:31 +0100

    add benches

--- benches/hash.rs
+++ benches/hash.rs
@@ -1,19 +1,41 @@
 use ark_bls12_381::Fr;
-use ark_relations::r1cs::ConstraintSystemRef;
+use ark_relations::r1cs::{ConstraintSystem, ConstraintSystemRef};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use liminal_ark_poseidon::circuit;
+use liminal_ark_poseidon::{circuit, hash};
 use manta_crypto::{
-    arkworks::constraint::{FpVar, R1CS},
+    arkworks::{
+        constraint::{fp::Fp, FpVar, R1CS},
+        ff::field_new,
+    },
     hash::ArrayHashFunction,
     rand::{OsRng, Sample},
 };
 use manta_pay::{
-    config::{poseidon::Spec2 as Poseidon2, utxo::InnerHashDomainTag},
+    config::{poseidon::Spec2 as Poseidon2, utxo::InnerHashDomainTag, ConstraintField},
     crypto::poseidon::hash::Hasher,
 };
 
 #[inline]
 fn poseidon_hash_manta(c: &mut Criterion) {
+    let mut group = c.benchmark_group("poseidon");
+    let mut rng = OsRng;
+    let hasher = black_box(Hasher::<Poseidon2, InnerHashDomainTag, 2>::sample(
+        (),
+        &mut rng,
+    ));
+    let inputs = black_box([
+        Fp(field_new!(ConstraintField, "1")),
+        Fp(field_new!(ConstraintField, "2")),
+    ]);
+    group.bench_function("manta", |b| {
+        b.iter(|| {
+            let _ = black_box(hasher.hash([&inputs[0], &inputs[1]], &mut ()));
+        })
+    });
+}
+
+#[inline]
+fn poseidon_hash_manta_circuit(c: &mut Criterion) {
     let mut group = c.benchmark_group("poseidon");
     let mut rng = OsRng;
     let hasher = black_box(Hasher::<Poseidon2, InnerHashDomainTag, 2, R1CS<_>>::sample(
@@ -25,7 +47,7 @@ fn poseidon_hash_manta(c: &mut Criterion) {
         FpVar::<Fr>::Constant(1.into()),
         FpVar::<Fr>::Constant(2.into()),
     ]);
-    group.bench_function("manta", |b| {
+    group.bench_function("manta_circuit", |b| {
         b.iter(|| {
             let _ = black_box(hasher.hash([&inputs[0], &inputs[1]], &mut compiler));
         })
@@ -36,11 +58,28 @@ fn poseidon_hash_manta(c: &mut Criterion) {
 fn poseidon_hash_liminal(c: &mut Criterion) {
     let mut group = c.benchmark_group("poseidon");
     let inputs = black_box([
+        field_new!(ConstraintField, "1"),
+        field_new!(ConstraintField, "2"),
+    ]);
+    group.bench_function("liminal", |b| {
+        b.iter(|| {
+            let _ = black_box(hash::two_to_one_hash([
+                inputs[0].clone(),
+                inputs[1].clone(),
+            ]));
+        })
+    });
+}
+
+#[inline]
+fn poseidon_hash_liminal_circuit(c: &mut Criterion) {
+    let mut group = c.benchmark_group("poseidon");
+    let inputs = black_box([
         FpVar::<Fr>::Constant(1.into()),
         FpVar::<Fr>::Constant(2.into()),
     ]);
-    let cs = black_box(ConstraintSystemRef::None);
-    group.bench_function("liminal", |b| {
+    let cs = black_box(ConstraintSystem::new_ref());
+    group.bench_function("liminal_circuit", |b| {
         b.iter(|| {
             let _ = black_box(circuit::two_to_one_hash(
                 cs.clone(),
@@ -50,5 +89,11 @@ fn poseidon_hash_liminal(c: &mut Criterion) {
     });
 }
 
-criterion_group!(crypto, poseidon_hash_manta, poseidon_hash_liminal);
+criterion_group!(
+    crypto,
+    poseidon_hash_manta_circuit,
+    poseidon_hash_manta,
+    poseidon_hash_liminal,
+    poseidon_hash_liminal_circuit
+);
 criterion_main!(crypto);
