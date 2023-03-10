72de8baf3822acae936908359ef8808c50bedcce
Michal Swietek <mike1729@users.noreply.github.com>
2023-03-10 13:25:41 +0100

    actually prove

--- benches/prover.rs
+++ benches/prover.rs
@@ -6,11 +6,11 @@ use manta_crypto::{
         constraint::{fp::Fp, FpVar, R1CS},
         ff::Fp256,
     },
-    constraint::measure::Measure,
+    constraint::ProofSystem,
     eclair::{
         alloc::{
             mode::{Public, Secret},
-            Allocate,
+            Allocate, Allocator,
         },
         bool::AssertEq,
     },
@@ -18,14 +18,58 @@ use manta_crypto::{
     rand::{OsRng, Sample},
 };
 use manta_pay::{
-    config::{poseidon::Spec2 as Poseidon2, utxo::InnerHashDomainTag, Compiler},
+    config::{
+        poseidon::Spec2 as Poseidon2, utxo::InnerHashDomainTag, Compiler, ProofSystem as mG16,
+    },
     crypto::poseidon::hash::Hasher,
 };
 use relations::{
-    Groth16, PreimageMantaRelationWithFullInput, PreimageRelationWithFullInput,
+    Groth16, LinearEquationRelationWithFullInput, LinearEquationRelationWithoutInput,
+    PreimageMantaRelationWithFullInput, PreimageRelationWithFullInput,
     PreimageRelationWithoutInput,
 };
 use wasmarking::Relation;
+
+#[inline]
+fn linear(c: &mut Criterion) {
+    let a = 2;
+    let b = 1;
+    let y = 3;
+    let circuit_withouth_input = LinearEquationRelationWithoutInput::new(a, b, y);
+    let x = 1;
+
+    let mut rng = ark_std::test_rng();
+    let (pk, _) = Groth16::circuit_specific_setup(circuit_withouth_input, &mut rng).unwrap();
+
+    c.bench_function("linear/liminal", |f| {
+        f.iter(|| {
+            // #constraints = 99
+            let full_circuit = LinearEquationRelationWithFullInput::new(a, b, y, x);
+
+            let _ = <Groth16 as SNARK<Fr>>::prove(&pk, full_circuit, &mut rng);
+        })
+    });
+}
+
+#[inline]
+fn linear_manta(c: &mut Criterion) {
+    // num constraints 99
+    let mut rng = OsRng;
+    let a = Fp(Fr::from(2));
+    let b = Fp(Fr::from(1));
+    let y = Fp(Fr::from(3));
+    c.bench_function("linear/liminal", |f| {
+        f.iter(|| {
+            // 241 constraints
+            let mut compiler = Compiler::for_proofs();
+            // let x: FpVar<Fp256<FrParameters>> = x.as_known::<Secret, _>(&mut compiler);
+            // let y: FpVar<Fp256<FrParameters>> = y.as_known::<Secret, _>(&mut compiler);
+            // let image: FpVar<Fp256<FrParameters>> = image.as_known::<Public, _>(&mut compiler);
+            // let hash_var = hasher_circuit.hash([&x, &y], &mut compiler);
+            // compiler.assert_eq(&hash_var, &image);
+        })
+    });
+}
 
 // TODO blockbox
 #[inline]
@@ -40,16 +84,23 @@ fn preimage_manta(c: &mut Criterion) {
     let y = Fp(Fr::from(2));
     let image = hasher.hash([&x, &y], &mut ());
     let hasher_circuit = Hasher::<Poseidon2, InnerHashDomainTag, 2, R1CS<_>>::sample((), &mut rng);
+    let mut compiler = mG16::context_compiler();
+    let xuk: FpVar<Fp256<FrParameters>> = compiler.allocate_unknown::<Secret, _>();
+    let yuk: FpVar<Fp256<FrParameters>> = compiler.allocate_unknown::<Secret, _>();
+    let iamgeuk: FpVar<Fp256<FrParameters>> = compiler.allocate_unknown::<Public, _>();
+    let hash_var_uk = hasher_circuit.hash([&xuk, &yuk], &mut compiler);
+    compiler.assert_eq(&hash_var_uk, &iamgeuk);
+    let (pk, _) = mG16::compile(&(), compiler, &mut rng).unwrap();
     group.bench_function("manta", |b| {
         b.iter(|| {
-            let mut compiler = Compiler::for_proofs();
-            // let x = compiler.allocate_known(&x);
+            // 241 constraints
+            let mut compiler = mG16::proof_compiler();
             let x: FpVar<Fp256<FrParameters>> = x.as_known::<Secret, _>(&mut compiler);
             let y: FpVar<Fp256<FrParameters>> = y.as_known::<Secret, _>(&mut compiler);
             let image: FpVar<Fp256<FrParameters>> = image.as_known::<Public, _>(&mut compiler);
             let hash_var = hasher_circuit.hash([&x, &y], &mut compiler);
             compiler.assert_eq(&hash_var, &image);
-            println!("num constraints: {}", compiler.constraint_count());
+            mG16::prove(&pk, compiler, &mut rng);
         })
     });
 }
@@ -133,6 +184,8 @@ criterion_group!(
     withdraw,
     preimage,
     preimage_manta,
-    preimage_poseidon_manta
+    preimage_poseidon_manta,
+    linear,
+    linear_manta
 );
 criterion_main!(prover);
