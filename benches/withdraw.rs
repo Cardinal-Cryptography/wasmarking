ba260cc5ee7dce1b21a259ba193fbb87b32dc408
Michal Swietek <mike1729@users.noreply.github.com>
2023-04-19 15:38:43 +0200

    proof&vk sizes

--- benches/withdraw.rs
+++ benches/withdraw.rs
@@ -1,3 +1,4 @@
+use ark_serialize::CanonicalSerialize;
 use criterion::{criterion_group, criterion_main, Criterion};
 use wasmarking::Relation;
 
@@ -7,16 +8,32 @@ fn bench(c: &mut Criterion) {
     c.bench_function("withdraw/keygen", |b| b.iter(|| relation.generate_keys()));
 
     let (pk, vk) = relation.generate_keys();
+    print_sizes("verify key", &vk);
 
     c.bench_function("withdraw/prover", |b| {
         b.iter(|| relation.generate_proof(pk.clone()))
     });
 
     let proof = relation.generate_proof(pk);
+    print_sizes("proof", &proof);
 
     c.bench_function("withdraw/verifier", |b| {
         b.iter(|| relation.verify_proof(&proof, &vk))
     });
+}
+
+fn print_sizes<T: CanonicalSerialize>(name: &str, obj: &T) {
+    let mut buf_compressed = vec![0; obj.serialized_size()];
+    obj.serialize(&mut buf_compressed[..]).unwrap();
+    let mut buf_uncompressed = vec![0; obj.uncompressed_size()];
+    obj.serialize_uncompressed(&mut buf_uncompressed[..])
+        .unwrap();
+    println!(
+        "{} size is: {} {}",
+        name,
+        buf_uncompressed.len(),
+        buf_compressed.len()
+    );
 }
 
 criterion_group!(withdraw, bench);
