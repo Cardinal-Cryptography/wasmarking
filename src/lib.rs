c3b6f41991d486e8f6f5e934e1459c77e67d1815
michal.swietek <michal.swietek@cardinals.cc>
2023-05-25 12:57:35 +0200

    single thread version

--- src/lib.rs
+++ src/lib.rs
@@ -2,42 +2,28 @@ use wasm_bindgen::prelude::wasm_bindgen;
 
 use crate::{
     js_api::{alert, now},
-    relation::Relation,
+    relation::JfRelation,
 };
 
 mod js_api;
 mod relation;
 
-/// Generate keys for a SNARK relation.
 #[wasm_bindgen]
-pub fn generate_keys(relation_name: &str) -> Vec<u8> {
+pub fn generate_proof() {
+    let relation = JfRelation::Withdraw;
+
+    alert("Generating srs");
+    let srs = relation.generate_srs();
+
+    alert("Generating keys");
+    let (pk, _) = relation.generate_keys(&srs);
+
+    alert("Generating proof");
     let start = now();
-
-    let relation = Relation::from(relation_name);
-    let pk = relation.generate_keys();
-
+    for _ in 0..1 {
+        relation.generate_proof(pk.clone());
+    }
     let elapsed = now() - start;
 
-    alert(&format!(
-        "Generating keys for `{relation_name}` took {elapsed}ms. Key has length: {}",
-        pk.len()
-    ));
-
-    pk
-}
-
-/// Generate a proof for a SNARK relation.
-#[wasm_bindgen]
-pub fn generate_proof(relation_name: &str, pk: Vec<u8>) {
-    let start = now();
-
-    let relation = Relation::from(relation_name);
-    let proof = relation.generate_proof(pk);
-
-    let elapsed = now() - start;
-
-    alert(&format!(
-        "Generating proof for `{relation_name}` took {elapsed}ms. Proof has length: {}",
-        proof.len()
-    ));
+    alert(&format!("Generating proof for took {elapsed}ms."));
 }
--- src/relation.rs
+++ src/relation.rs
@@ -1,96 +1,95 @@
-use ark_bls12_381::Bls12_381;
-use ark_groth16::Groth16;
-use ark_serialize::CanonicalDeserialize;
-use ark_snark::SNARK;
-use ark_std::test_rng;
-use relations::{serialize, WithdrawRelation, XorRelationWithFullInput, XorRelationWithoutInput};
+use ark_ff::PrimeField;
+use jf_primitives::{
+    merkle_tree::{
+        prelude::RescueSparseMerkleTree, MerkleCommitment, MerkleTreeScheme,
+        UniversalMerkleTreeScheme,
+    },
+    pcs::prelude::UnivariateUniversalParams,
+};
+use jf_relations::{
+    generate_srs,
+    shielder_types::{compute_note, convert_array},
+    withdraw::{WithdrawPrivateInput, WithdrawPublicInput, WithdrawRelation},
+    Curve, Proof as JfProof, ProvingKey as JfProvingKey, Relation as _,
+    VerifyingKey as JfVerifyingKey,
+};
+use num_bigint::BigUint;
 
-pub enum Relation {
-    Xor,
+pub enum JfRelation {
     Withdraw,
 }
 
-impl From<&str> for Relation {
-    fn from(value: &str) -> Self {
-        match value {
-            "xor" => Self::Xor,
-            "withdraw" => Self::Withdraw,
-            _ => unreachable!("Unknown relation"),
-        }
+impl JfRelation {
+    pub fn generate_srs(&self) -> UnivariateUniversalParams<Curve> {
+        let rng = &mut jf_utils::test_rng();
+        generate_srs(17_000, rng).unwrap()
+    }
+
+    pub fn generate_keys(
+        &self,
+        srs: &UnivariateUniversalParams<Curve>,
+    ) -> (JfProvingKey<Curve>, JfVerifyingKey<Curve>) {
+        WithdrawRelation::generate_keys(srs).unwrap()
+    }
+
+    pub fn generate_proof(&self, pk: JfProvingKey<Curve>) -> JfProof<Curve> {
+        let rng = &mut jf_utils::test_rng();
+        let relation = jf_relation();
+        relation.generate_proof(&pk, rng).unwrap()
     }
 }
 
-impl Relation {
-    pub fn generate_keys(&self) -> Vec<u8> {
-        let mut rng = test_rng();
+fn jf_relation() -> WithdrawRelation {
+    let token_id = 1;
+    let whole_token_amount = 10;
+    let spend_trapdoor = [1; 4];
+    let spend_nullifier = [2; 4];
+    let spend_note = compute_note(
+        token_id,
+        whole_token_amount,
+        spend_trapdoor,
+        spend_nullifier,
+    );
 
-        let (pk, _vk) = match self {
-            Relation::Xor => Groth16::<Bls12_381>::circuit_specific_setup(
-                XorRelationWithoutInput::new(2),
-                &mut rng,
-            ),
-            Relation::Withdraw => Groth16::<Bls12_381>::circuit_specific_setup(
-                WithdrawRelation::without_input(16),
-                &mut rng,
-            ),
-        }
-        .unwrap();
+    let deposit_token_amount = 7;
+    let deposit_trapdoor = [3; 4];
+    let deposit_nullifier = [4; 4];
+    let deposit_note = compute_note(
+        token_id,
+        deposit_token_amount,
+        deposit_trapdoor,
+        deposit_nullifier,
+    );
 
-        serialize(&pk)
-    }
+    let leaf_index = 0u64;
+    let uid = BigUint::from(leaf_index);
+    let elem = convert_array(spend_note);
+    let mt = RescueSparseMerkleTree::from_kv_set(11, &[(uid.clone(), elem)]).unwrap();
+    let (retrieved_elem, merkle_proof) = mt.lookup(&uid).expect_ok().unwrap();
+    assert_eq!(retrieved_elem, elem);
+    assert!(mt.verify(&uid, merkle_proof.clone()).expect("succeed"));
+    let merkle_root = mt.commitment().digest().into_bigint().0;
 
-    pub fn generate_proof(&self, pk: Vec<u8>) -> Vec<u8> {
-        let pk = CanonicalDeserialize::deserialize(&*pk).unwrap();
+    let public_input = WithdrawPublicInput {
+        fee: 1,
+        recipient: [7; 32],
+        token_id,
+        spend_nullifier,
+        token_amount_out: 3,
+        merkle_root,
+        deposit_note,
+    };
 
-        let mut rng = test_rng();
-        let proof = match self {
-            Relation::Xor => {
-                Groth16::<Bls12_381>::prove(&pk, XorRelationWithFullInput::new(2, 1, 3), &mut rng)
-            }
-            Relation::Withdraw => {
-                let circuit = WithdrawRelation::with_full_input(
-                    16,
-                    10,
-                    [
-                        212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214,
-                        130, 44, 133, 88, 133, 76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125,
-                    ],
-                    0,
-                    1919191919,
-                    [1, 2, 3, 4],
-                    100,
-                    [0, 0, 0, 0],
-                    17171717171717,
-                    181818181818,
-                    41414141414141,
-                    vec![
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                    ],
-                    65537,
-                    [0, 0, 0, 0],
-                    200,
-                    100,
-                );
-                Groth16::<Bls12_381>::prove(&pk, circuit, &mut rng)
-            }
-        }
-        .unwrap();
+    let private_input = WithdrawPrivateInput {
+        spend_trapdoor,
+        deposit_trapdoor,
+        deposit_nullifier,
+        merkle_proof,
+        leaf_index,
+        spend_note,
+        whole_token_amount,
+        deposit_token_amount,
+    };
 
-        serialize(&proof)
-    }
+    WithdrawRelation::new(public_input, private_input)
 }
