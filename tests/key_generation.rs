1bb882cc406e740920d2f9d0b3b19b9d9b1e6271
michal.swietek <michal.swietek@cardinals.cc>
2023-05-26 13:53:47 +0200

    jf-withdraw

--- tests/key_generation.rs
+++ tests/key_generation.rs
@@ -1,14 +1,14 @@
 use instant::Instant;
 use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
-use wasmarking::Relation;
+use wasmarking::{ArkRelation, JfRelation};
 use web_sys::console;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 static REPEAT: usize = 3;
 
-fn bench(relation_name: &str) {
-    let relation = Relation::from(relation_name);
+fn ark_bench(relation_name: &str) {
+    let relation = ArkRelation::from(relation_name);
 
     let start = Instant::now();
     for _ in 0..REPEAT {
@@ -27,11 +27,48 @@ fn bench(relation_name: &str) {
 }
 
 #[wasm_bindgen_test]
-fn bench_xor() {
-    bench("xor");
+fn ark_bench_xor() {
+    ark_bench("xor");
 }
 
 #[wasm_bindgen_test]
-fn bench_withdraw() {
-    bench("withdraw");
+fn ark_bench_withdraw() {
+    ark_bench("withdraw");
+}
+
+fn jf_bench(operation: &str, job: impl Fn()) {
+    let start = Instant::now();
+    for _ in 0..REPEAT {
+        job()
+    }
+    let elapsed = Instant::now().duration_since(start);
+
+    console::log_1(
+        &format!(
+            "{:?} key generation. Relation performance: {:?}",
+            operation,
+            (elapsed / REPEAT as u32)
+        )
+        .into(),
+    );
+}
+
+#[wasm_bindgen_test]
+fn jf_bench_withdraw() {
+    let relation = JfRelation::Withdraw;
+
+    let job = || relation.generate_circuit();
+    jf_bench("circuit generation", job);
+
+    let job = || {
+        let _ = relation.generate_srs();
+    };
+    jf_bench("srs generation", job);
+
+    let srs = relation.generate_srs();
+
+    let job = || {
+        let _ = relation.generate_keys(&srs);
+    };
+    jf_bench("keys generation", job);
 }
--- tests/prover.rs
+++ tests/prover.rs
@@ -1,15 +1,15 @@
 use instant::Instant;
 use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
-use wasmarking::Relation;
+use wasmarking::{ArkRelation, JfRelation};
 use web_sys::console;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 static REPEAT: usize = 3;
 
-fn bench(relation_name: &str) {
-    let relation = Relation::from(relation_name);
-    let pk = relation.generate_keys();
+fn bench_ark(relation_name: &str) {
+    let relation = ArkRelation::from(relation_name);
+    let (pk, _) = relation.generate_keys();
 
     let start = Instant::now();
     for _ in 0..REPEAT {
@@ -28,11 +28,33 @@ fn bench(relation_name: &str) {
 }
 
 #[wasm_bindgen_test]
-fn bench_xor() {
-    bench("xor");
+fn bench_ark_xor() {
+    bench_ark("xor");
 }
 
 #[wasm_bindgen_test]
-fn bench_withdraw() {
-    bench("withdraw");
+fn bench_ark_withdraw() {
+    bench_ark("withdraw");
+}
+
+#[wasm_bindgen_test]
+fn bench_jf_withdraw() {
+    let relation = JfRelation::Withdraw;
+    let srs = relation.generate_srs();
+    let (pk, _) = relation.generate_keys(&srs);
+
+    let start = Instant::now();
+    for _ in 0..REPEAT {
+        relation.generate_proof(pk.clone());
+    }
+    let elapsed = Instant::now().duration_since(start);
+
+    console::log_1(
+        &format!(
+            "{:?} prover. Relation performance: {:?}",
+            "jf_withdraw",
+            (elapsed / REPEAT as u32)
+        )
+        .into(),
+    );
 }
